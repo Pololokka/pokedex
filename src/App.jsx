d5c307d345e61a840eb0b85bf10fa105446c1d00
Pololokka <vitorbulbovas1@hotmail.com>
2023-04-20 16:06:04 -0300

    :construction: fazendo main

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import "./App.css";
 function App() {
   return (
     <>
+      <header className="header">
+        <h1 className="title font-hover">Encontre o seu Pokémon!</h1>
+      </header>
       <main className="App"></main>
     </>
   );
