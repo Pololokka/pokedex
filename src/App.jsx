3eb9bcb5be87b01e13061e948deb132b4bc3f47d
Pololokka <vitorbulbovas1@hotmail.com>
2023-04-20 16:50:16 -0300

    :lipstick: feat: página principal estilizada

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,29 @@ function App() {
       <header className="header">
         <h1 className="title font-hover">Encontre o seu Pokémon!</h1>
       </header>
-      <main className="App"></main>
+      <main className="App">
+        <section className="pkm__container">
+          <label htmlFor="pkm-search" className="subtitle font-hover">
+            Digite o nome do Pokémon
+          </label>
+          <input type="text" name="pkm-search" className="text pkm__search" />
+          <div className="pkm__show">
+            <div className="pkm__itens">
+              <img
+                src="src/assets/test-img.png"
+                alt="Imagem do Pokemon"
+                className="pkm-image"
+              />
+              <div className="pkm__stats">
+                <p className="text text-hover">Nome: Rhidon</p>
+                <p className="text text-hover">Altura: 1,90m</p>
+                <p className="text text-hover">Peso: 120kg</p>
+              </div>
+            </div>
+            <hr className="line"></hr>
+          </div>
+        </section>
+      </main>
     </>
   );
 }
